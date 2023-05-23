664cb4b1424f377b1e0aae2e5b5251a57d5d8571
guilherme <guilherme121225@gmail.com>
2023-05-23 16:19:56 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3333;
+
+app.post("/users", (request, response) => {
+    const { name, email, password } = request.body;
+
+    response.json({ name, email, password });
+})
+
+app.listen (PORT, () => console.log(`Server is running on port ${PORT}`));
