c2bcc89fef41abec97939cab5f519023bb61484a
guilherme <guilherme121225@gmail.com>
2023-05-25 11:20:59 -0300

    migrations

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 require("express-async-errors");
+const migrationsRun = require("./database/sqlite/migrations")
 
 const AppError = require("./utils/AppError");
 const express = require('express');
 
 const routes = require("./routes");
+migrationsRun();
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
+
 
 app.use((error, request, response, next)=>{
     if(error instanceof AppError){
