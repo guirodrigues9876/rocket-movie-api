cd1bc7c99fa5e7a538def685a4852da7e7cd49b8
Guilherme Rodrigues <guilherme121225@gmail.com>
2023-08-04 10:53:57 -0300

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps : [{
+    name: "app",
+    script: "./src/server.js",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }, {
+     name: 'worker',
+     script: 'worker.js'
+  }]
+}
